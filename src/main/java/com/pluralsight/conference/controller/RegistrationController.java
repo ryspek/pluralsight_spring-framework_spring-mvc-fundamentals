f7e30f734a648bfaae88e7ad63bd34bd2e75860a
Ryspek Baibosunov <ryspek.baybosunov@gmail.com>
2022-04-19 21:30:16 +0600

    add modelAttribute for passing form parameters

--- src/main/java/com/pluralsight/conference/controller/RegistrationController.java
+++ src/main/java/com/pluralsight/conference/controller/RegistrationController.java
@@ -1,16 +1,24 @@
 package com.pluralsight.conference.controller;
 
-import java.util.Map;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.pluralsight.conference.model.Registration;
 
 @Controller
 public class RegistrationController {
 	
 	@GetMapping("registration")
-	public String getRegistration(Map<String, Object> model) {
+	public String getRegistration(@ModelAttribute ("registration") Registration registration) {
 		
+		return "registration";
+	}
+	
+	@PostMapping("registration")
+	public String addRegistration(@ModelAttribute ("registration") Registration registration) {
+		System.out.println("Registration: " + registration.getName());
 		return "registration";
 	}
 
--- src/main/webapp/WEB-INF/jsp/registration.jsp
+++ src/main/webapp/WEB-INF/jsp/registration.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>   
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,5 +9,22 @@
 </head>
 <body>
 	<h1>Registration</h1>
+	<form:form modelAttribute="registration">
+		<table>
+			<tr>
+				<td>
+					Name:
+				</td>
+				<td>
+					<form:input path="name" />
+				</td>
+			</tr>
+			<tr>
+				<td colspan="2">
+					<input type="submit" value="Add Registration">
+				</td>
+			</tr>		
+		</table>
+	</form:form>
 </body>
 </html>
