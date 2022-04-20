94284c11933646fb2139d9c9be15f427a1442db7
Ryspek Baibosunov <ryspek.baybosunov@gmail.com>
2022-04-20 23:23:31 +0600

    add view resolver

---
+++ src/main/java/com/pluralsight/conference/ConferenceConfig.java
@@ -0,0 +1,20 @@
+package com.pluralsight.conference;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+public class ConferenceConfig {
+
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver bean = new InternalResourceViewResolver();
+		bean.setPrefix("/WEB-INF/jsp/");
+		bean.setSuffix(".jsp");
+		bean.setOrder(0);
+		return bean;
+	}
+	
+}
